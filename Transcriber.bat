e4f789c98ffa3864a133a42f997af81dfd7589bc
ZEE <white.noise616@gmail.com>
2024-06-03 17:59:51 -0600

    fixed bat

--- Transcriber.bat
+++ Transcriber.bat
@@ -1,6 +1,17 @@
 @echo off
-cd /d %~dp0
-echo Starting the Flask server...
-start /B python app.py
-echo Opening the browser...
+
+IF NOT EXIST env (
+    echo Creating virtual environment...
+    python -m venv env
+    call env\Scripts\activate
+    echo Installing dependencies...
+    pip install -r requirements.txt
+) ELSE (
+    echo Activating existing virtual environment...
+    call env\Scripts\activate
+)
+
+echo Starting Flask app...
+start cmd /k "python app.py"
+timeout /t 10
 start http://127.0.0.1:5000
